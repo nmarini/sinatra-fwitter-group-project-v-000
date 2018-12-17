4d6634f9355dec79db0f5a2095469a044d3e2d3f
Nicolas Marini <nmarini44@gmail.com>
2018-12-17 03:31:58 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -38,7 +38,7 @@ class TweetsController < ApplicationController
   get '/tweets/:id/edit' do
     if logged_in?
       @tweet = Tweet.find_by(params[:id])
-      if @tweet.user_id == current_user.id
+      if current_user.tweets.cinlude?(@tweet)
         erb :'/tweets/edit_tweets'
       else
         redirect '/tweets'
