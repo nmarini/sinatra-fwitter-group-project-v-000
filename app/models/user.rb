7c7984ea95cfed2819e7cfc669680bdbda5dbefc
Nicolas Marini <nmarini44@gmail.com>
2018-12-17 04:01:30 +0000

    completed project

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  has_many :tweets
+
+  has_secure_password
+
+  def slug
+    self.username.downcase.gsub(" ", "-")
+
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find{|u| u.slug == slug }
+  end
+
+end
